dc12d7e329beb6db1f1c57659522efe1b8934062
Lunar Starstrum <lunar@forsaken-borders.net>
2022-06-08 14:03:18 -0500

    Download dependencies

--- src/Reflection/DownloadNightlies.cs
+++ src/Reflection/DownloadNightlies.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
@@ -10,17 +9,19 @@ using System.Runtime.Versioning;
 using System.Text.Json;
 using System.Threading.Tasks;
 using System.Xml;
+using System.Xml.Linq;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.Interactivity;
 using DSharpPlus.Lavalink;
 using DSharpPlus.SlashCommands;
 using DSharpPlus.VoiceNext;
+using NuGet.Frameworks;
 
 namespace DSharpPlus.DocBot
 {
     public partial class CachedReflection
     {
-        private static readonly TargetFrameworkAttribute currentFramework = Assembly.GetExecutingAssembly().GetCustomAttribute<TargetFrameworkAttribute>()!;
+        private static readonly NuGetFramework CurrentFramework = NuGetFramework.Parse(Assembly.GetExecutingAssembly().GetCustomAttribute<TargetFrameworkAttribute>()!.FrameworkName);
         private static readonly HttpClient HttpClient = new()
         {
             DefaultRequestHeaders = { { "User-Agent", "DSharpPlus Docs Bot/2.0" } }
@@ -108,7 +109,7 @@ namespace DSharpPlus.DocBot
             Environment.CurrentDirectory = Path.GetFullPath("DSharpPlus Nightlies");
             foreach (string fileName in Directory.GetFiles(Environment.CurrentDirectory, "*.nupkg").OrderBy(x => x))
             {
-                string extractedDllName = await UnpackNupkgAsync(fileName) + ".dll";
+                string extractedDllName = await UnpackNupkgAsync(fileName);
                 Console.WriteLine("Attempting to load: " + Path.GetFullPath(extractedDllName));
                 Assembly assembly = Assembly.LoadFrom(Path.GetFullPath(extractedDllName));
                 types.AddRange(assembly.GetExportedTypes());
@@ -119,61 +120,42 @@ namespace DSharpPlus.DocBot
             SetProperties();
         }
 
-        private static async Task<string> UnpackNupkgAsync(string nupkgFile)
+        private static async Task<string> UnpackNupkgAsync(string nupkgFile, NuGetFramework? nuGetFramework = null)
         {
-            string dll = Path.GetFullPath(Path.ChangeExtension(nupkgFile, ".dll"));
-            string symbols = Path.GetFullPath(Path.ChangeExtension(nupkgFile, ".snupkg"));
-            string dependencies = Path.GetFullPath(Path.ChangeExtension(nupkgFile, ".nuspec"));
             string? assemblyName = null;
-            List<string> irresponsiblyNamedFileList = new();
+            nuGetFramework ??= CurrentFramework;
 
             using ZipArchive archive = ZipFile.Open(nupkgFile, ZipArchiveMode.Read);
+            string[] frameworks = archive.Entries.Where(x => x.FullName.StartsWith("lib/", StringComparison.OrdinalIgnoreCase)).Select(x => x.FullName.Split('/')[1]).Distinct().ToArray();
+            NuGetFramework specificFramework = new FrameworkReducer().GetNearest(nuGetFramework, frameworks.Select(NuGetFramework.Parse));
+
             foreach (ZipArchiveEntry entry in archive.Entries)
             {
-                // TODO: Extract the DLL first, then the symbols, then the dependencies, all under the base ProductName
-                if (entry.FullName.EndsWith(".dll"))
+                string entryExtension = Path.GetExtension(entry.Name);
+                switch (entryExtension)
                 {
-                    entry.ExtractToFile(dll);
-                    FileVersionInfo fileVersionInfo = FileVersionInfo.GetVersionInfo(dll);
-                    File.Move(dll, fileVersionInfo.ProductName + ".dll");
-                    assemblyName = fileVersionInfo.ProductName;
+                    case ".xml":
+                    case ".dll":
+                        if (entry.FullName.StartsWith("lib/") && NuGetFramework.Parse(entry.FullName.Split('/')[1]) == specificFramework)
+                        {
+                            assemblyName = Path.ChangeExtension(entry.Name, ".dll");
+                            entry.ExtractToFile(entry.Name);
+                        }
+                        break;
+                    case ".nupkg":
+                    case ".snupkg":
+                    case ".nuspec":
+                        entry.ExtractToFile(entry.Name);
+                        break;
                 }
-                else if (entry.FullName.EndsWith(".snupkg"))
-                {
-                    if (assemblyName == null)
-                    {
-                        irresponsiblyNamedFileList.Add(symbols);
-                        entry.ExtractToFile(symbols);
-                    }
-                    else
-                    {
-                        entry.ExtractToFile(assemblyName + ".snupkg");
-                    }
-                }
-                else if (entry.FullName.EndsWith(".nuspec"))
-                {
-                    if (assemblyName == null)
-                    {
-                        irresponsiblyNamedFileList.Add(dependencies);
-                        entry.ExtractToFile(dependencies);
-                    }
-                    else
-                    {
-                        entry.ExtractToFile(assemblyName + ".nuspec");
-                    }
-
-                }
-            }
-
-            foreach (string poorlyNamedFile in irresponsiblyNamedFileList)
-            {
-                File.Move(poorlyNamedFile, assemblyName + Path.GetExtension(poorlyNamedFile));
             }
 
             File.Delete(nupkgFile);
-            if (File.Exists(assemblyName + ".nuspec"))
+            string? nuspecFile = Path.ChangeExtension(assemblyName, ".nuspec");
+            if (nuspecFile != null && File.Exists(nuspecFile))
             {
-                await ResolveDependenciesAsync(assemblyName + ".nuspec");
+                await ResolveDependenciesAsync(nuspecFile);
+                File.Delete(nuspecFile);
             }
             return assemblyName!;
         }
@@ -181,44 +163,65 @@ namespace DSharpPlus.DocBot
         private static async Task ResolveDependenciesAsync(string nuspecFile)
         {
             // Parse dependencies
-            using XmlReader reader = XmlReader.Create(nuspecFile);
-            if (reader.ReadToFollowing("dependencies")) // If the nupkg has dependencies
+            XDocument nuspecDoc = XDocument.Load(XmlReader.Create(nuspecFile));
+            XElement? dependencies = nuspecDoc.Element(XName.Get("package", nuspecDoc.Root!.GetDefaultNamespace().NamespaceName))?
+                .Element(XName.Get("metadata", nuspecDoc.Root!.GetDefaultNamespace().NamespaceName))?
+                .Element(XName.Get("dependencies", nuspecDoc.Root!.GetDefaultNamespace().NamespaceName));
+            if (dependencies == null) // If the nupkg has no dependencies
             {
-                XmlReader groupNodes = reader.ReadSubtree();
-                groupNodes.Read();
-                while (groupNodes.Read())
+                return;
+            }
+
+            List<NuGetFramework> frameworks = new();
+            XmlReader reader = dependencies.CreateReader();
+            reader.Read();
+            while (reader.Read())
+            {
+                string? targetFramework = reader.GetAttribute("targetFramework");
+                if (targetFramework != null)
                 {
-                    if (groupNodes.Name == "")
+                    frameworks.Add(NuGetFramework.Parse(targetFramework));
+                }
+
+                continue;
+            }
+
+            NuGetFramework? requiredFramework = new FrameworkReducer().GetNearest(CurrentFramework, frameworks);
+            reader = dependencies.CreateReader();
+            reader.Read();
+            while (reader.Read())
+            {
+                string? targetFramework = reader.GetAttribute("targetFramework");
+                if (targetFramework == null || NuGetFramework.Parse(targetFramework) != requiredFramework)
+                {
+                    continue;
+                }
+
+                XmlReader dependencyNodes = reader.ReadSubtree();
+                dependencyNodes.Read();
+                while (dependencyNodes.Read())
+                {
+                    if (dependencyNodes.Name == "" || dependencyNodes.NodeType == XmlNodeType.EndElement)
                     {
                         continue;
                     }
 
-                    //string? targetFramework = reader.GetAttribute("targetFramework");
-                    XmlReader dependencyNodes = reader.ReadSubtree();
-                    dependencyNodes.Read();
-                    while (dependencyNodes.Read())
+                    string? dependencyId = dependencyNodes.GetAttribute("id");
+                    if (dependencyId == null || File.Exists(dependencyId + ".dll") || File.Exists(dependencyId + ".nuspec"))
                     {
-                        if (dependencyNodes.Name == "")
-                        {
-                            continue;
-                        }
-
-                        string dependencyId = dependencyNodes.GetAttribute("id")!.ToLowerInvariant();
-                        if (File.Exists(dependencyId + ".nupkg"))
-                        {
-                            continue;
-                        }
-
-                        string dependencyVersion = dependencyNodes.GetAttribute("version")!;
-                        string dependencyUrl = $"https://api.nuget.org/v3-flatcontainer/{dependencyId}/{dependencyVersion}/{dependencyId}.{dependencyVersion}.nupkg";
-                        Console.WriteLine($"Fetching {dependencyId} v{dependencyVersion}");
-                        FileStream zipFile = File.OpenWrite(dependencyId + ".nupkg");
-                        (await HttpClient.GetStreamAsync(dependencyUrl)).CopyTo(zipFile);
-                        zipFile.Close();
-
-                        await UnpackNupkgAsync(dependencyId + ".nupkg");
-                        File.Delete(dependencyId + ".nupkg");
+                        continue;
                     }
+
+                    dependencyId = dependencyId.ToLowerInvariant();
+                    string dependencyVersion = dependencyNodes.GetAttribute("version")!;
+                    string dependencyUrl = $"https://api.nuget.org/v3-flatcontainer/{dependencyId}/{dependencyVersion}/{dependencyId}.{dependencyVersion}.nupkg";
+                    Console.WriteLine($"Fetching {dependencyId} v{dependencyVersion}");
+                    FileStream zipFile = File.OpenWrite(dependencyId + ".nupkg");
+                    (await HttpClient.GetStreamAsync(dependencyUrl)).CopyTo(zipFile);
+                    zipFile.Close();
+
+                    await UnpackNupkgAsync(dependencyId + ".nupkg", requiredFramework);
+                    File.Delete(dependencyId + ".nupkg");
                 }
             }
         }
@@ -249,7 +252,7 @@ namespace DSharpPlus.DocBot
             // Gather all D#+ methods, grouping them by method name and grouping them by method overloads
             MethodGroups = Types
                 .SelectMany(type => type
-                    .GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static) // TODO: I'd like to use binding flags here as I feel it'd be more efficient however I haven't been able to understand how they work yet.
+                    .GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static)
                     .Where(method =>
                         !method.IsSpecialName && // Drop these stupid getters and setters.
                         (method.GetBaseDefinition().DeclaringType?.Namespace?.StartsWith("DSharpPlus") ?? false))) // Drop methods not implemented by us (aka object and Enum methods)
